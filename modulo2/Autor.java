2b2d3be634ef98a43ed4b42c20c987bb58574cca
saubor <163947257+saubor@users.noreply.github.com>
2025-08-08 11:53:16 -0300

    Update Autor.java

--- modulo2/Autor.java
+++ modulo2/Autor.java
@@ -3,9 +3,11 @@ import java.util.List;
 
 public class Autor extends Pessoa {
     private List<Livro> livrosEscritos = new ArrayList<>();
+    private boolean ehUsuario;
 
-    public Autor(String nome, int idade) {
+    public Autor(String nome, int idade, boolean ehUsuario) {
         super(nome, idade);
+        this.ehUsuario = ehUsuario;
     }
 
     public void adicionarLivro(Livro livro) {
@@ -14,5 +16,13 @@ public class Autor extends Pessoa {
 
     public List<Livro> getLivrosEscritos() {
         return livrosEscritos;
+    }
+
+    public boolean isEhUsuario() {
+        return ehUsuario;
+    }
+
+    public void setEhUsuario(boolean ehUsuario) {
+        this.ehUsuario = ehUsuario;
     }
 }
