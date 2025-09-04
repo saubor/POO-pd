b0d2c6a825fae2cc5fc8f4177f9110ed9593f2d5
saubor <163947257+saubor@users.noreply.github.com>
2025-09-03 23:09:19 -0300

    Update AutorTest.java

--- modulo3/AutorTest.java
+++ modulo3/AutorTest.java
@@ -1,12 +1,27 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AutorTest {
+class AutorTest {
+
     @Test
-    void testEhUsuario() {
+    void testPublicacaoLivro() {
+        Autor autor = new Autor("Jessica Felix", 34, false);
+        autor.setEstrategiaPublicacao(new EstrategiaPublicacaoLivro());
+
+        assertDoesNotThrow(() -> autor.publicar("Java Avançado"));
+    }
+
+    @Test
+    void testPublicacaoArtigo() {
         Autor autor = new Autor("Alan Turing", 41, true);
-        assertTrue(autor.isEhUsuario());
-        autor.setEhUsuario(false);
-        assertFalse(autor.isEhUsuario());
+        autor.setEstrategiaPublicacao(new EstrategiaPublicacaoArtigo());
+
+        assertDoesNotThrow(() -> autor.publicar("Compiladores Modernos"));
+    }
+
+    @Test
+    void testSemEstrategia() {
+        Autor autor = new Autor("Outro Autor", 30, false);
+        assertDoesNotThrow(() -> autor.publicar("Nada acontece")); // imprime aviso
     }
 }
